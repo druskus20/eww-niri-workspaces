7c53fdaa7f51cf2a43a83fe30d3abc523ca8e614
druskus20 <dev@druskus.com>
2025-03-30 20:18:09 +0200

    feat: first implementation

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,124 @@
-use niri_ipc::{socket::Socket, Event, Response};
-use std::{collections::HashMap, io};
+use niri_ipc::{socket::Socket, Event, Window, WindowLocation, Workspace};
+
+mod serializable;
 
 fn main() {
     let mut state = State::new();
     let niri_socket_env = std::env::var("NIRI_SOCKET");
-    let mut niri_socket = if *&niri_socket_env.is_ok() {
-        Socket::connect_to(&niri_socket_env.unwrap()).unwrap()
+    let connection = if let Ok(niri_socket) = niri_socket_env {
+        Socket::connect_to(niri_socket).unwrap()
     } else {
         Socket::connect().unwrap()
     };
-    let r = niri_socket.send(niri_ipc::Request::EventStream);
-    let (_, mut f) = handle_niri_response(r).unwrap();
-
-    loop {
-        let event = f().unwrap();
-        state.update_with_event(event);
+    let (r, mut block_read_next_event) = connection.send(niri_ipc::Request::EventStream).unwrap();
+    match r {
+        Ok(_) => loop {
+            let event = block_read_next_event().unwrap();
+            dbg!(get_event_name_str(&event));
+            state.update_with_event(event);
+            serializable::SerializableState::from(&state);
+            let json =
+                serde_json::to_string(&serializable::SerializableState::from(&state)).unwrap();
+            dbg!(json);
+        },
+        Err(e) => {
+            eprintln!("Niri error: {}", e);
+            std::process::exit(1);
+        }
     }
-    todo!()
 }
 
-type Id = u64;
+fn get_event_name_str(e: &Event) -> &'static str {
+    match e {
+        Event::WorkspacesChanged { .. } => "WorkspacesChanged",
+        Event::WorkspaceActivated { .. } => "WorkspaceActivated",
+        Event::WorkspaceActiveWindowChanged { .. } => "WorkspaceActiveWindowChanged",
+        Event::WindowsChanged { .. } => "WindowsChanged",
+        Event::WindowOpenedOrChanged { .. } => "WindowOpenedOrChanged",
+        Event::WindowClosed { .. } => "WindowClosed",
+        Event::WindowFocusChanged { .. } => "WindowFocusChanged",
+        Event::WindowsLocationsChanged { .. } => "WindowsLocationsChanged",
+        Event::KeyboardLayoutsChanged { .. } => "KeyboardLayoutsChanged",
+        Event::KeyboardLayoutSwitched { .. } => "KeyboardLayoutSwitched",
+    }
+}
 
+#[derive(Debug, Default)]
 struct State {
-    _windows_per_workspace: HashMap<Id, Vec<Id>>,
-    focused_workspace_id: Id,
-    active_workspaces_id: Vec<Id>,
-    focused_window_id: Id,
+    workspaces: Vec<Workspace>,
+    windows: Vec<Window>,
 }
 
 impl State {
     fn new() -> Self {
-        Self {
-            _windows_per_workspace: Default::default(),
-            focused_workspace_id: Default::default(),
-            active_workspaces_id: Default::default(),
-            focused_window_id: Default::default(),
-        }
+        Self::default()
     }
 
-    fn update_with_event(&mut self, e: Event) {}
-}
+    /// https://yalter.github.io/niri/niri_ipc/enum.Event.html
+    fn update_with_event(&mut self, e: Event) {
+        match e {
+            Event::WorkspacesChanged { workspaces } => self.workspaces = workspaces,
+            Event::WorkspaceActivated { id, focused } => {
+                if focused {
+                    // All other workspaces become not focused
+                    for workspace in self.workspaces.iter_mut() {
+                        workspace.is_focused = false;
+                    }
+                }
+                if let Some(workspace) = self.workspaces.iter_mut().find(|w| w.id == id) {
+                    workspace.is_active = true;
+                    workspace.is_focused = focused;
+                }
+            }
+            Event::WorkspaceActiveWindowChanged {
+                workspace_id,
+                active_window_id,
+            } => {
+                if let Some(workspace) = self.workspaces.iter_mut().find(|w| w.id == workspace_id) {
+                    workspace.active_window_id = active_window_id;
+                }
+            }
+            Event::WindowsChanged { windows } => self.windows = windows,
+            Event::WindowOpenedOrChanged { window } => {
+                if window.is_focused {
+                    // All other windows become not focused
+                    for window in self.windows.iter_mut() {
+                        window.is_focused = false;
+                    }
+                }
 
-fn handle_niri_response(
-    r: Result<
-        (
-            Result<Response, String>,
-            impl FnMut() -> Result<Event, std::io::Error>,
-        ),
-        std::io::Error,
-    >,
-) -> Result<(Response, impl FnMut() -> io::Result<Event>), String> {
-    match r {
-        Ok((Ok(r), f)) => Ok((r, f)),
-        Ok((Err(e), _)) => Err(e),
-        Err(e) => Err(e.to_string()),
+                // Change or add window
+                if let Some(w) = self.windows.iter_mut().find(|w| w.id == window.id) {
+                    *w = window;
+                } else {
+                    self.windows.push(window);
+                }
+            }
+            Event::WindowClosed { id } => {
+                self.windows.retain(|w| w.id != id);
+            }
+            Event::WindowFocusChanged { id } => {
+                // All other windows become not focused
+                for window in self.windows.iter_mut() {
+                    window.is_focused = false;
+                }
+
+                // If a window is meant to be focused
+                if let Some(id) = id {
+                    if let Some(window) = self.windows.iter_mut().find(|w| w.id == id) {
+                        window.is_focused = true;
+                    }
+                }
+            }
+            Event::WindowsLocationsChanged { changes } => {
+                for (id, window_location) in changes {
+                    if let Some(window) = self.windows.iter_mut().find(|w| w.id == id) {
+                        window.location = window_location;
+                    }
+                }
+            }
+            Event::KeyboardLayoutsChanged { .. } => { /* Do nothing */ }
+            Event::KeyboardLayoutSwitched { .. } => { /* Do nothing */ }
+        }
     }
 }
---
+++ src/serializable.rs
@@ -0,0 +1,92 @@
+use std::collections::HashMap;
+
+/// Serializable state ready to be consumed by Eww as Json
+///
+/// The json should have: Output -> Workspaces -> Windows { positons }
+use serde::Serialize;
+
+use crate::State;
+
+#[derive(Serialize)]
+pub(crate) struct SerializableState {
+    outputs: HashMap<String, Output>,
+}
+
+#[derive(Serialize)]
+struct Output {
+    workspaces: HashMap<u64, Workspace>,
+}
+#[derive(Serialize)]
+struct Workspace {
+    id: u64,
+    windows: Vec<Window>,
+    is_active: bool,
+}
+
+#[derive(Serialize)]
+struct Window {
+    id: u64,
+    column: usize,
+    is_focused: bool,
+}
+
+impl From<&State> for SerializableState {
+    fn from(state: &State) -> Self {
+        // first create the workspaces - without windows, then populate the windows
+        let mut outputs = HashMap::<String, Output>::new();
+        for workspace in state.workspaces.iter() {
+            let output_name = if let Some(output) = &workspace.output {
+                output
+            } else {
+                continue;
+            };
+
+            let output = outputs
+                .entry(output_name.clone())
+                .or_insert_with(|| Output {
+                    workspaces: HashMap::new(),
+                });
+
+            output.workspaces.insert(
+                workspace.id,
+                Workspace {
+                    id: workspace.id,
+                    windows: Vec::new(),
+                    is_active: workspace.is_active,
+                },
+            );
+        }
+
+        // populate the windows
+        for window in state.windows.iter() {
+            // We only care about non-floating windows
+            if window.is_floating {
+                continue;
+            }
+            // We only care about windows with a workspace (that exists)
+            let workspace = match window.workspace_id {
+                Some(workspace_id) => outputs
+                    .values_mut()
+                    .flat_map(|output| output.workspaces.values_mut())
+                    .find(|workspace| workspace.id == workspace_id)
+                    .expect("Workspace id set for window not found in state's workspaces"),
+                None => continue,
+            };
+
+            let column = window
+                .location
+                .tile_pos_in_scrolling_layout
+                .expect(
+                    "Tile position not set, something is wrong, non-floating windows should have a tile position",
+                )
+                .0;
+            workspace.windows.push(Window {
+                id: window.id,
+                column: window.location.tile_pos_in_scrolling_layout.unwrap().0,
+                is_focused: window.is_focused,
+            });
+        }
+
+        SerializableState { outputs }
+    }
+}
